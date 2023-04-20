10306775042ab9bb7ab614d982015ad94f086a06
Gerardo <angel.orozco.gzl@gmail.com>
2023-04-20 17:13:09 -0500

    require

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
+require('dotenv').config()
+
 let titulo = document.getElementById('titulo')
 titulo.innerHTML = `Api Key = ${process.env.API_KEY}`
-require('dotenv').config()
 console.log(`Api Key = ${process.env.API_KEY}`)
